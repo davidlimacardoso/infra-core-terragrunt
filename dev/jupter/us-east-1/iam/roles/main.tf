eada5c0b1aa13239259079ba8470deeeb2634658
davidlimacardoso <david.lima.cd@gmail.com>
2024-10-03 11:19:10 -0300

    Add IAM roles

---
+++ dev/jupter/us-east-1/iam/roles/main.tf
@@ -0,0 +1,32 @@
+variable "roles" {
+  description = "List of IAM roles to create"
+  type = list(object({
+    name               = string
+    description        = string
+    assume_role_policy = string
+    policy             = string
+  }))
+}
+
+resource "aws_iam_policy" "create" {
+  for_each    = { for role in var.roles : role.name => role }
+  name        = "${replace(join("-", [for part in split("-", each.value.name) : title(part)]), "-", "")}Policy"
+  description = "Policy - ${each.value.description}"
+  policy      = each.value.policy
+}
+
+resource "aws_iam_role" "create" {
+  for_each = { for role in var.roles : role.name => role }
+  name = "${replace(join("-", [for part in split("-", each.value.name) : title(part)]), "-", "")}Role" #Remove hyphen and make upper case first chars and join all words ex: Ec2InstanceS3ConnectRole
+  assume_role_policy = each.value.assume_role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  for_each = { for role in var.roles : role.name => role }
+  policy_arn = aws_iam_policy.create[each.key].arn
+  role       = aws_iam_role.create[each.key].name
+}
+
+output "roles" {
+  value = { for role in aws_iam_role.create : role.name => role.arn }
+}
---
+++ dev/jupter/us-east-1/iam/roles/terragrunt.hcl
@@ -0,0 +1,52 @@
+terraform {
+  source = "."
+}
+
+include {
+  path = find_in_parent_folders()
+}
+
+inputs = {
+  roles = [
+    {
+      name = "ec2-instance-s3-connect"
+      description = "Role for EC2 instance to connect to S3"
+      assume_role_policy = jsonencode(
+        {
+          "Version" : "2012-10-17",
+          "Statement" : [
+            {
+              "Effect" : "Allow",
+              "Action" : [
+                "sts:AssumeRole"
+              ],
+              "Principal" : {
+                "Service" : [
+                  "ec2.amazonaws.com"
+                ]
+              }
+            }
+          ]
+        }
+      )
+
+      policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+          {
+            Effect = "Allow"
+            Action = [
+              "s3:GetObject",
+              "s3:PutObject",
+              "s3:ListBucket"
+            ]
+            Resource = [
+              "arn:aws:s3:::vprofile-dev-artifacts",
+              "arn:aws:s3:::vprofile-dev-artifacts/*"
+            ]
+          }
+        ]
+      })
+    }
+  ]
+}
