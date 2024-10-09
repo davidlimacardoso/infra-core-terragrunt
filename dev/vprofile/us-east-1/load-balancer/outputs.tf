900c9139816ba8a463adf1d325520766d5491275
davidlimacardoso <david.lima.cd@gmail.com>
2024-10-09 17:27:47 -0300

    Improved outputs loadbalancer

--- dev/vprofile/us-east-1/load-balancer/outputs.tf
+++ dev/vprofile/us-east-1/load-balancer/outputs.tf
@@ -1,3 +1,8 @@
 output "domain_name" {
-  value = zipmap(values(aws_lb.create_lb).*.dns_name, values(aws_lb.create_lb).*.dns_name) #{ for alb in var.alb : alb.name => aws_lb.create_lb[alb.name].dns_name }
+  value = {
+    for alb in var.alb : alb.name => {
+      dns_name = aws_lb.create_lb[alb.name].dns_name
+      arn      = aws_lb.create_lb[alb.name].arn
+    }
+  }
 }
