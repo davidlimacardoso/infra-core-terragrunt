d0cb6f366a592056bcb91bc2f7b29bc183ea14f0
davidlimacardoso <david.lima.cd@gmail.com>
2024-10-09 10:41:58 -0300

    Add ec2 instances

---
+++ dev/vprofile/us-east-1/ec2/key.tf
@@ -0,0 +1,23 @@
+
+# Generate a secure private key for the EC2 instance
+# and save it as a file on disk
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+resource "tls_private_key" "create_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "null_resource" "save_key" {
+  provisioner "local-exec" {
+    command     = <<-EOT
+        echo '${tls_private_key.create_key.private_key_pem}' > ../../../${var.key_pair_name}.pem
+    EOT
+    interpreter = ["bash", "-c"]
+  }
+}
+
+# Create a key-pair for the EC2 instance
+resource "aws_key_pair" "create_key" {
+  key_name   = var.key_pair_name
+  public_key = tls_private_key.create_key.public_key_openssh
+}
