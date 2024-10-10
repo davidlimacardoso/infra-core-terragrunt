4fca6887b233fa733a3a87d27b26dcff43184d57
davidlimacardoso <david.lima.cd@gmail.com>
2024-10-10 08:46:48 -0300

    Add outputs private ip of ec2 instances

--- dev/vprofile/us-east-1/ec2/outputs.tf
+++ dev/vprofile/us-east-1/ec2/outputs.tf
@@ -10,3 +10,7 @@ output "key_pair_name" {
 output "iam_instance_profile" {
   value = zipmap(values(aws_iam_instance_profile.ec2_instance_profile).*.name, values(aws_iam_instance_profile.ec2_instance_profile).*.arn)
 }
+
+output "ec2_private_ip" {
+  value = { for k, instance in aws_instance.create_instance : k => { instance = instance.id, private_ip = instance.private_ip } }
+}
