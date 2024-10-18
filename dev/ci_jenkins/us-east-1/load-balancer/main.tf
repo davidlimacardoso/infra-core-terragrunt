2dd9e71e26239e9b07540b7fb90d53aed08af0f0
davidlimacardoso <david.lima.cd@gmail.com>
2024-10-17 21:58:03 -0300

    Allow http listener

--- dev/ci_jenkins/us-east-1/load-balancer/main.tf
+++ dev/ci_jenkins/us-east-1/load-balancer/main.tf
@@ -52,14 +52,14 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-# resource "aws_lb_listener" "http" {
-#   for_each          = { for alb in var.alb : alb.name => alb }
-#   load_balancer_arn = aws_lb.create_lb[each.key].arn
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "http" {
+  for_each          = { for alb in var.alb : alb.name => alb }
+  load_balancer_arn = aws_lb.create_lb[each.key].arn
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.create_lb_tg[each.key].arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.create_lb_tg[each.key].arn
+  }
+}
