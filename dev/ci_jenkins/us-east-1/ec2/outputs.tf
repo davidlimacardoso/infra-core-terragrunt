ca8f03aed574c00d04eee78061e378b31c42f3e9
davidlimacardoso <david.lima.cd@gmail.com>
2024-11-06 14:50:17 -0300

    Add ec2 outputs instance and public ip

--- dev/ci_jenkins/us-east-1/ec2/outputs.tf
+++ dev/ci_jenkins/us-east-1/ec2/outputs.tf
@@ -14,3 +14,14 @@ output "key_pair_name" {
 output "ec2_private_ip" {
   value = { for k, instance in aws_instance.create_instance : k => { instance = instance.id, private_ip = instance.private_ip } }
 }
+
+
+output "instances" {
+  value = {
+    for k, instance in aws_instance.create_instance : k => {
+      id         = instance.id
+      private_ip = instance.private_ip
+      public_ip  = instance.public_ip
+    }
+  }
+}
